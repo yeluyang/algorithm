49534f27cc2e49cd6ce3c6e69d763c3237b3505a
yeluyang <ylycpg@gmail.com>
2022-01-11 15:48:41 +0800

    test[play/rust]: add unit test

--- playground/rust/src/solution.rs
+++ playground/rust/src/solution.rs
@@ -1,6 +1,17 @@
 pub fn solution() {
     // call solution here
-    println!("{:?}", unimplemented!())
+    println!("{:?}", unimplemented!());
 }
 
 // impl solution here
+
+// imple test case for solution here
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solution() {
+        unimplemented!();
+    }
+}
