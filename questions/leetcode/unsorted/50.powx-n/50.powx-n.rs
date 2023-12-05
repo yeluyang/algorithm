31431d859eaa79c8419ccc1440c42119a340d989
yeluyang <ylycpg@gmail.com>
2023-12-05 20:15:10 +0800

    refactor(quest/lc): update 50.powx-n with better solution

--- questions/leetcode/unsorted/50.powx-n/50.powx-n.rs
+++ questions/leetcode/unsorted/50.powx-n/50.powx-n.rs
@@ -55,26 +55,48 @@
 
 // @lc code=start
 impl Solution {
+    // 递归
+    // pub fn my_pow(x: f64, n: i32) -> f64 {
+    //     if n < 0 {
+    //         Self::_my_pow(1.0 / x, -n)
+    //     } else {
+    //         Self::_my_pow(x, n)
+    //     }
+    // }
+    // fn _my_pow(x: f64, n: i32) -> f64 {
+    //     match n {
+    //         0 => 1.0,
+    //         1 => x,
+    //         _ => {
+    //             let mut r = Self::_my_pow(x, n / 2);
+    //             r *= r;
+    //             if n % 2 != 0 {
+    //                 r *= x;
+    //             }
+    //             r
+    //         }
+    //     }
+    // }
+    // 迭代
     pub fn my_pow(x: f64, n: i32) -> f64 {
-        if n < 0 {
-            Self::_my_pow(1.0 / x, -n)
+        let (mut x, n) = if n < 0 {
+            (1.0 / x, !(n as u32) + 1)
         } else {
-            Self::_my_pow(x, n)
-        }
-    }
-    fn _my_pow(x: f64, n: i32) -> f64 {
-        match n {
-            0 => 1.0,
-            1 => x,
-            _ => {
-                let mut r = Self::_my_pow(x, n / 2);
-                r *= r;
-                if n % 2 != 0 {
-                    r *= x;
-                }
-                r
+            (x, n as u32)
+        };
+        let mut r = 1.0;
+        let mut m = 1;
+        for _ in 0..32 {
+            if m > n {
+                break;
             }
+            if (n & m) > 0 {
+                r *= x;
+            }
+            x *= x;
+            m <<= 1;
         }
+        r
     }
 }
 // @lc code=end
