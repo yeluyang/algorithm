931177167230d3861b2fc40740881d322fb4508c
yeluyang <ylycpg@gmail.com>
2024-02-03 10:26:30 +0800

    feat(play/rs): support leetcode tree parsing

--- playground/rust/src/leetcode.rs
+++ playground/rust/src/leetcode.rs
@@ -1,4 +1,4 @@
-use std::{cell::RefCell, rc::Rc};
+use std::{cell::RefCell, collections::VecDeque, rc::Rc};
 
 // Definition for a binary tree node.
 #[derive(Debug, PartialEq, Eq)]
@@ -17,6 +17,62 @@ impl TreeNode {
             right: None,
         }
     }
+}
+
+fn options_from_str(s: &str) -> Vec<Option<i32>> {
+    serde_json::from_str(s).unwrap()
+}
+
+fn tree_from_vec(v: Vec<Option<i32>>) -> Option<Rc<RefCell<TreeNode>>> {
+    if v.is_empty() {
+        return None;
+    }
+    let mut iter = v.into_iter();
+    let mut q = VecDeque::new();
+    let mut root = None;
+    if let Some(first) = iter.next().unwrap() {
+        root = Some(Rc::new(RefCell::new(TreeNode::new(first))));
+        q.push_back(root.clone());
+    } else {
+        return None;
+    }
+
+    while let Some(node) = q.pop_front() {
+        if iter.is_empty() {
+            break;
+        }
+        if node.is_none() {
+            iter.nth(2);
+            q.push_back(None);
+            q.push_back(None);
+            continue;
+        }
+
+        let node = node.unwrap();
+
+        let mut left = None;
+        if let Some(val) = iter.next().unwrap() {
+            left.replace(Rc::new(RefCell::new(TreeNode::new(val))));
+            node.borrow_mut().left = left.clone();
+        }
+        q.push_back(left);
+
+        if iter.is_empty() {
+            break;
+        }
+        let mut right = None;
+        if let Some(val) = iter.next().unwrap() {
+            right.replace(Rc::new(RefCell::new(TreeNode::new(val))));
+            node.borrow_mut().right = right.clone();
+        }
+        q.push_back(right);
+    }
+
+    root
+}
+
+pub fn tree_from_str(s: &str) -> Option<Rc<RefCell<TreeNode>>> {
+    tree_from_vec(options_from_str(s))
 }
 
 // Definition for singly-linked list.
--- playground/rust/src/lib.rs
+++ playground/rust/src/lib.rs
@@ -1,5 +1,8 @@
+#![feature(exact_size_is_empty)]
+
 #[macro_use]
 extern crate log;
+extern crate serde_json;
 
 mod leetcode;
 pub mod solution;
