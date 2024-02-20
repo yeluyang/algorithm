1b028c78a1193659b21a8d7bc723fb7633f23176
yeluyang <ylycpg@gmail.com>
2024-02-20 09:28:28 +0800

    feat(q/lc): ac 329.longest-increasing-path-in-a-matrix

--- questions/leetcode/unsorted/329.longest-increasing-path-in-a-matrix.rs
+++ questions/leetcode/unsorted/329.longest-increasing-path-in-a-matrix.rs
@@ -56,6 +56,104 @@
 
 // @lc code=start
 impl Solution {
-    pub fn longest_increasing_path(matrix: Vec<Vec<i32>>) -> i32 {}
+    pub fn longest_increasing_path(matrix: Vec<Vec<i32>>) -> i32 {
+        if matrix.is_empty() {
+            return 0;
+        }
+        Graph::new(&matrix).longest_increasing_path()
+    }
+}
+
+struct Node {
+    longest_path: std::cell::RefCell<i32>,
+    adjacent_nodes: Vec<usize>,
+}
+
+impl Node {
+    fn new() -> Self {
+        Self {
+            longest_path: std::cell::RefCell::new(0),
+            adjacent_nodes: vec![],
+        }
+    }
+}
+
+struct Graph {
+    nodes: Vec<Node>,
+    longest_path: std::cell::RefCell<i32>,
+}
+
+impl Graph {
+    fn new(matrix: &Vec<Vec<i32>>) -> Self {
+        if matrix.is_empty() {
+            return Self {
+                nodes: Vec::with_capacity(0),
+                longest_path: std::cell::RefCell::new(1),
+            };
+        }
+        let rows = matrix.len();
+        let cols = matrix[0].len();
+        let mut graph = Self {
+            nodes: Vec::with_capacity(rows * cols),
+            longest_path: std::cell::RefCell::new(1),
+        };
+        for row in 0..rows {
+            for col in 0..cols {
+                let mut node = Node::new();
+                if let Some(r) = row.checked_sub(1) {
+                    if matrix[row][col] < matrix[r][col] {
+                        node.adjacent_nodes.push(r * cols + col);
+                    }
+                }
+                if row + 1 < rows {
+                    if matrix[row][col] < matrix[row + 1][col] {
+                        node.adjacent_nodes.push((row + 1) * cols + col);
+                    }
+                }
+                if let Some(c) = col.checked_sub(1) {
+                    if matrix[row][col] < matrix[row][c] {
+                        node.adjacent_nodes.push(row * cols + c);
+                    }
+                }
+                if col + 1 < cols {
+                    if matrix[row][col] < matrix[row][col + 1] {
+                        node.adjacent_nodes.push(row * cols + col + 1);
+                    }
+                }
+
+                graph.nodes.push(node);
+            }
+        }
+        graph
+    }
+
+    fn dfs_update_longest_path(&self, indicate: usize) {
+        let cur = &self.nodes[indicate];
+        if *cur.longest_path.borrow() != 0 {
+            return;
+        }
+        cur.longest_path.replace(1);
+        for i in 0..self.nodes[indicate].adjacent_nodes.len() {
+            let next_i = self.nodes[indicate].adjacent_nodes[i];
+            let next = &self.nodes[next_i];
+            if *next.longest_path.borrow() == 0 {
+                self.dfs_update_longest_path(next_i);
+            };
+            let next_max = *next.longest_path.borrow() + 1;
+            if next_max > *cur.longest_path.borrow() {
+                cur.longest_path.replace(next_max);
+                if *cur.longest_path.borrow() > *self.longest_path.borrow() {
+                    self.longest_path.replace(*cur.longest_path.borrow());
+                };
+            };
+        }
+    }
+
+    fn longest_increasing_path(&self) -> i32 {
+        for i in 0..self.nodes.len() {
+            self.dfs_update_longest_path(i);
+        }
+        *self.longest_path.borrow()
+    }
 }
 // @lc code=end
